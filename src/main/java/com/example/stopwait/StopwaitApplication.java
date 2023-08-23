1fbdd65c01b5950fd94f0bb980b086fed62e425a
s2s22 <sycho3713@hanmail.net>
2023-08-24 00:41:12 +0900

    동기화 커밋

---
+++ src/main/java/com/example/stopwait/StopwaitApplication.java
@@ -0,0 +1,14 @@
+package com.example.stopwait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopwaitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StopwaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stopwait/StopwaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stopwait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopwaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
